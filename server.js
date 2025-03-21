de032d90f009acf1464f14352aae62d9fca40203
siduna lemma <sidunalemma@gmail.com>
2025-03-21 13:41:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 
-const path = require('path');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
